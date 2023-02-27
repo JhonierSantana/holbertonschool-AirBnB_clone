7c14ad74ba0140976f1f356d64f4aed294993722
root <root@DESKTOP-ISQ43BP>
2023-02-27 12:07:16 -0500

    upgrade

--- models/base_model.py
+++ models/base_model.py
@@ -20,15 +20,27 @@ class BaseModel:
     save(self):
     to_dict(self):
     """
-    def Base(self, id):
-        self.id = id
-        id = uuid.uuid4()
-        self.id_string = str(id)
+    def __init__(self, *args, **kwards):
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
+        if kwards:
+            if "__class__" in kwards.keys():
     
     def __str__(self):
-        return ([BaseModel]({}).format(self.id, self.__dict__))
+        return "[{}] ({}) {}".format(self.__class__.__name__,\
+            self.id, self.__dict__)
         
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
+        
+    def to_dict(self):
+        dict1 = dict()
+        for (key, value) in (self.__init__).items:
+            if isinstance(value, datetime):
+                dict1[key] = value.isoformat()
+            else:
+                dict1[key] = value
+        dict1["__class__"] = self.__class__.__name__
+        return(dict1)
