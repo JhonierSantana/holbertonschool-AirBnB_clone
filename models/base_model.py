f0335606b1b6c69c019410842bedaaa7419cc16f
luiluma <luma24lopez@gmail.com>
2023-02-27 12:29:17 -0500

    prueba 2

--- models/base_model.py
+++ models/base_model.py
@@ -20,13 +20,21 @@ class BaseModel:
     save(self):
     to_dict(self):
     """
-    def __init__(self, *args, **kwards):
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            kwargs.pop("__class__", None)
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+            else:
+                setattr(self, key, value)
+                
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if kwards:
-            if "__class__" in kwards.keys():
-    
+        models.storage(self)
+
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__,\
             self.id, self.__dict__)
