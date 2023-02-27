3ba4149477ad5de6a11adc96633103f9fef438ea
luiluma <luma24lopez@gmail.com>
2023-02-27 12:34:22 -0500

    prueba 3

--- models/base_model.py
+++ models/base_model.py
@@ -30,6 +30,7 @@ class BaseModel:
             else:
                 setattr(self, key, value)
                 
+        else:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
