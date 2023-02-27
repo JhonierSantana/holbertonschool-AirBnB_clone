ea7fbde21aeb7c41ba131ef23ed9067d3e484689
root <root@DESKTOP-ISQ43BP>
2023-02-27 11:30:04 -0500

    upgrade

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-import uuid
-import models
 from datetime import datetime
-from os import path
+import models
+import uuid
 
 
 class BaseModel:
     def Base(self, id):
         self.id = id
         id = uuid.uuid4()
-        self.id_string = str(id)
+        self.id = str(id)
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
     
     def __str__(self):
-        return ([BaseModel]({}).format(self.id, self.__dict__))
-    
-    @property
-    def updated_at(self):
-        return self.__updated_at
-    
-    @updated_at.setter
-    def updated_at(self, valor):
-        self.__updated_at = datetime.now()
-        
+        return "[{}] ({}) {}".format(self.__class__.__name__,\
+            self.id, self.__dict__)
+
     def save(self):
         self.updated_at = datetime.now()
-        
-    #def to_dict(self):
-        
+    
