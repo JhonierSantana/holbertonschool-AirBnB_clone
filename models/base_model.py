f6e2cb511febbc2fa9548cb7e788cd9bec89c92a
root <root@DESKTOP-ISQ43BP>
2023-02-27 12:30:35 -0500

    upgrade

--- models/base_model.py
+++ models/base_model.py
@@ -20,13 +20,11 @@ class BaseModel:
     save(self):
     to_dict(self):
     """
-    def __init__(self, *args, **kwards):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if kwards:
-            if "__class__" in kwards.keys():
-    
+        
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__,\
             self.id, self.__dict__)
