3a716c7cd60143b872ac9a6cfd74856974ee244b
luiluma <luma24lopez@gmail.com>
2023-03-03 17:34:07 -0500

    documentation of methods in tests city and place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -7,26 +7,35 @@ import models
 
 
 class TestPlace(unittest.TestCase):
-    """ Testing Place """
+    """
+    Testing Place
+    """
     def test_instance(self):
-        """ test instance """
+        """
+        test instance
+        """
         test1 = Place()
         self.assertIsInstance(test1, Place)
 
     def test_permissions(self):
-        """ test permissions """
+        """
+        test permissions
+        """
         self.assertTrue(os.access("models/place.py", os.X_OK))
         self.assertTrue(os.access("models/place.py", os.R_OK))
         self.assertTrue(os.access("models/place.py", os.W_OK))
         self.assertTrue(os.access("models/place.py", os.F_OK))
 
     def test_ids_maker(self):
-        """ test to generate unique id """
+        """test to generate unique id
+        """
         place1_id = Place()
         place2_id = Place()
         self.assertNotEqual(place1_id, place2_id)
 
     def test_docstring_mandatory(self):
-        """ testing if class has correct docstring """
+        """
+        testing if class has correct docstring
+        """
         self.assertIsNotNone(models.place.__doc__)
         self.assertIsNotNone(Place.__doc__)
