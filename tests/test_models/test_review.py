1be3731d744d18810dd029334a9a155294faed06
luiluma <luma24lopez@gmail.com>
2023-03-03 17:36:46 -0500

    documentation of methods in tests state and review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -7,26 +7,36 @@ import models
 
 
 class TestCity(unittest.TestCase):
-    """ Testing Review """
+    """
+    Testing Review
+    """
     def test_instance(self):
-        """ test instance """
+        """
+        test instance
+        """
         test1 = Review()
         self.assertIsInstance(test1, Review)
 
     def test_permissions(self):
-        """ test permissions """
+        """
+        test permissions
+        """
         self.assertTrue(os.access("models/review.py", os.X_OK))
         self.assertTrue(os.access("models/review.py", os.R_OK))
         self.assertTrue(os.access("models/review.py", os.W_OK))
         self.assertTrue(os.access("models/review.py", os.F_OK))
 
     def test_ids_maker(self):
-        """ test to generate unique id """
+        """
+        test to generate unique id
+        """
         review1_id = Review()
         review2_id = Review()
         self.assertNotEqual(review1_id, review2_id)
 
     def test_docstring_mandatory(self):
-        """ testing if class has correct docstring """
+        """
+        testing if class has correct docstring
+        """
         self.assertIsNotNone(models.review.__doc__)
         self.assertIsNotNone(Review.__doc__)
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -8,26 +8,36 @@ import datetime
 
 
 class TestState(unittest.TestCase):
-    """ Testing State """
+    """
+    Testing State
+    """
     def test_instance(self):
-        """ test instance """
+        """
+        test instance
+        """
         test1 = State()
         self.assertIsInstance(test1, State)
 
     def test_permissions(self):
-        """ test permissions """
+        """
+        test permissions
+        """
         self.assertTrue(os.access("models/state.py", os.X_OK))
         self.assertTrue(os.access("models/state.py", os.R_OK))
         self.assertTrue(os.access("models/state.py", os.W_OK))
         self.assertTrue(os.access("models/state.py", os.F_OK))
 
     def test_ids_maker(self):
-        """ test to generate the id """
+        """
+        test to generate the id
+        """
         state1_id = State()
         state2_id = State()
         self.assertNotEqual(state1_id, state2_id)
 
     def test_docstring_mandatory(self):
-        """ testing if class has correct docstring """
+        """
+        testing if class has correct docstring
+        """
         self.assertIsNotNone(models.state.__doc__)
         self.assertIsNotNone(State.__doc__)
